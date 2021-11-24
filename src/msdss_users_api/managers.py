bc0995701989ca2e3544e484d2ec59aa1acddcb1
Richard Wen <rrwen.dev@gmail.com>
2021-11-23 23:37:43 -0500

    Working on refactor

--- src/msdss_users_api/managers.py
+++ src/msdss_users_api/managers.py
@@ -8,16 +8,30 @@ from .models import UserCreate, UserDB
 
 class Environment:
     """
-    Environment
+    Class to manage environmental variables.
+
+    Parameters
+    ----------
+    env_file : str
+        The path of the file with environmental variables.
+    key_path : str
+        The path of the key file for the ``env_file``.
+    **kwargs
+        Keyword arguments defining the environmental attributes and variable names for this object.
+
+        * Each key represents the attribute name, and each value represents the environmental variable name
+        * For example, passing ``secret='MSDSS_SECRET'`` sets an attribute ``secret`` and a method ``get_secret``, which gets the environmental variable from the ``env_file``
+        * The method can also accept a single default value, if the variable does not exist in the ``env_file``
 
     Example
     -------
     .. jupyter-execute::
 
-        from msdss_users_api.models import *
-        from pprint import pprint
+        from msdss_base_dotenv import Environment
 
-        pprint(dir(UserManager))
+        env = Environment(secret='MSDSS_SECRET')
+        secret = env.get_secret('default')
+        print(secret)
     """
     def __init__(
         self,
@@ -29,7 +43,11 @@ class Environment:
         for k, v in kwargs.items():
             setattr(self, k, v)
             get_method = partial(lambda default=None: os.getenv(getattr(self, k), default))
+            set_method = partial(lambda value: set_env_var(getattr(self, v), value))
+            del_method = partial(lambda value: del_env_var(getattr(self, v)))
             setattr(self, 'get_' + k, get_method)
+            setattr(self, 'set_' + k, set_method)
+            setattr(self, 'del_' + k, del_method)
         
         # (EnvironmentManager_attrs) Set standard attrs
         self.env_file = env_file
